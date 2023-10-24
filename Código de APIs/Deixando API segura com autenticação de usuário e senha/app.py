6c073665a5b9d2cc5c0fc6d4c8fc2662712abc4a
DanielTrombetta08 <145787463+DanielTrombetta08@users.noreply.github.com>
2023-10-24 07:28:42 -0300

    24/10

--- Código de APIs/Deixando API segura com autenticação de usuário e senha/app.py
+++ Código de APIs/Deixando API segura com autenticação de usuário e senha/app.py
@@ -1,50 +1,11 @@
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, jsonify, request
 from estrutura_banco_de_dados import Autor, Postagem, app, db
 import json
-import jwt
-from datetime import datetime, timedelta
-from functools import wraps
 # Rota padrão - GET https://localhost:5000
 
 
-def token_obrigatorio(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        token = None
-        # Verificar se um token foi enviado
-        if 'x-access-token' in request.headers:
-            token = request.headers['x-access-token']
-        if not token:
-            return jsonify({'mensagem': 'Token não foi incluído!'}, 401)
-        # Se temos um token, validar acesso consultando o BD
-        try:
-            resultado = jwt.decode(token,app.config['SECRET_KEY'],algorithms=["HS256"])
-            autor = Autor.query.filter_by(
-                id_autor=resultado['id_autor']).first()
-        except:
-            return jsonify({'mensagem': 'Token é inválido'}, 401)
-        return f(autor, *args, **kwargs)
-    return decorated
-
-
-@app.route('/login')
-def login():
-    auth = request.authorization
-    if not auth or not auth.username or not auth.password:
-        return make_response('Login inválido', 401, {'WWW-Authenticate': 'Basic realm="Login obrigatório"'})
-    usuario = Autor.query.filter_by(nome=auth.username).first()
-    if not usuario:
-        return make_response('Login inválido', 401, {'WWW-Authenticate': 'Basic realm="Login obrigatório"'})
-    if auth.password == usuario.senha:
-        token = jwt.encode({'id_autor': usuario.id_autor, 'exp': datetime.utcnow(
-        ) + timedelta(minutes=30)}, app.config['SECRET_KEY'])
-        return jsonify({'token':token})
-    return make_response('Login inválido', 401, {'WWW-Authenticate': 'Basic realm="Login obrigatório"'})
-
-
 @app.route('/')
-@token_obrigatorio
-def obter_postagens(autor):
+def obter_postagens():
     postagens = Postagem.query.all()
 
     list_postagens = []
@@ -59,8 +20,7 @@ def obter_postagens(autor):
 
 
 @app.route('/postagem/<int:id_postagem>', methods=['GET'])
-@token_obrigatorio
-def obter_postagem_por_indice(autor, id_postagem):
+def obter_postagem_por_indice(id_postagem):
     postagem = Postagem.query.filter_by(id_postagem=id_postagem).first()
     postagem_atual = {}
     try:
@@ -75,8 +35,7 @@ def obter_postagem_por_indice(autor, id_postagem):
 
 
 @app.route('/postagem', methods=['POST'])
-@token_obrigatorio
-def nova_postagem(autor):
+def nova_postagem():
     nova_postagem = request.get_json()
     postagem = Postagem(
         titulo=nova_postagem['titulo'], id_autor=nova_postagem['id_autor'])
@@ -90,8 +49,7 @@ def nova_postagem(autor):
 
 
 @app.route('/postagem/<int:id_postagem>', methods=['PUT'])
-@token_obrigatorio
-def alterar_postagem(autor, id_postagem):
+def alterar_postagem(id_postagem):
     postagem_alterada = request.get_json()
     postagem = Postagem.query.filter_by(id_postagem=id_postagem).first()
     try:
@@ -110,8 +68,7 @@ def alterar_postagem(autor, id_postagem):
 
 
 @app.route('/postagem/<int:id_postagem>', methods=['DELETE'])
-@token_obrigatorio
-def excluir_postagem(autor, id_postagem):
+def excluir_postagem(id_postagem):
     postagem_a_ser_excluida = Postagem.query.filter_by(
         id_postagem=id_postagem).first()
     if not postagem_a_ser_excluida:
@@ -123,8 +80,7 @@ def excluir_postagem(autor, id_postagem):
 
 
 @app.route('/autores')
-@token_obrigatorio
-def obter_autores(autor):
+def obter_autores():
     autores = Autor.query.all()
     lista_de_autores = []
     for autor in autores:
@@ -138,8 +94,7 @@ def obter_autores(autor):
 
 
 @app.route('/autores/<int:id_autor>', methods=['GET'])
-@token_obrigatorio
-def obter_autor_por_id(autor, id_autor):
+def obter_autor_por_id(id_autor):
     autor = Autor.query.filter_by(id_autor=id_autor).first()
     if not autor:
         return jsonify(f'Autor não encontrado!')
@@ -154,8 +109,7 @@ def obter_autor_por_id(autor, id_autor):
 
 
 @app.route('/autores', methods=['POST'])
-@token_obrigatorio
-def novo_autor(autor):
+def novo_autor():
     print('deu merda')
     novo_autor = request.get_json()
     autor = Autor(
@@ -168,8 +122,7 @@ def novo_autor(autor):
 
 
 @ app.route('/autores/<int:id_autor>', methods=['PUT'])
-@token_obrigatorio
-def alterar_autor(autor, id_autor):
+def alterar_autor(id_autor):
     usuario_a_alterar = request.get_json()
     autor = Autor.query.filter_by(id_autor=id_autor).first()
     if not autor:
@@ -192,8 +145,7 @@ def alterar_autor(autor, id_autor):
 
 
 @ app.route('/autores/<int:id_autor>', methods=['DELETE'])
-@token_obrigatorio
-def excluir_autor(autor, id_autor):
+def excluir_autor(id_autor):
     autor_existente = Autor.query.filter_by(id_autor=id_autor).first()
     if not autor_existente:
         return jsonify({'mensagem': 'Este autor não foi encontrado'})
---
+++ modulo 9/token_de_autenticacao.md
@@ -0,0 +1,5 @@
+# Construindo uma API para baixar Papeis de Parede Premium
+# papeldeparede.com/dowload/1
+# Autenticação por TOKEN JWT
+# Token = 'SH@@@$#HSHD1235'
+# papeldeparede.com/login
