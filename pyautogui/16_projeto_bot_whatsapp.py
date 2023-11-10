53a568bd4f2f162fe7ec700b494ab93ca35de800
DanielTrombetta08 <145787463+DanielTrombetta08@users.noreply.github.com>
2023-11-10 06:37:05 -0300

    Update 16_projeto_bot_whatsapp.py

--- pyautogui/16_projeto_bot_whatsapp.py
+++ pyautogui/16_projeto_bot_whatsapp.py
@@ -6,10 +6,10 @@ telefones = [5548999454534]
 
 for telefone in telefones:
     webbrowser.open(f'https://api.whatsapp.com/send?phone={telefone}')
-    sleep(10)
+"""     sleep(10)
     pyautogui.click(1512,208,duration=1)
     sleep(10)
     pyautogui.typewrite('Opa, ignora essa mensagem. To testando uma automação de um robô! hehe')
     sleep(5)
     pyautogui.press('enter')
-    sleep(300)
+    sleep(300) """
