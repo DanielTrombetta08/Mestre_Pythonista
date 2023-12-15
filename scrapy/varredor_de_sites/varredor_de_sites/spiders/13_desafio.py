ecb7bf7b37d0e169279cf1722034e5046ca15062
DanielTrombetta08 <145787463+DanielTrombetta08@users.noreply.github.com>
2023-12-15 06:47:17 -0300

    15/12

--- scrapy/varredor_de_sites/varredor_de_sites/spiders/13_desafio.py
+++ scrapy/varredor_de_sites/varredor_de_sites/spiders/13_desafio.py
@@ -1,45 +1,4 @@
 import scrapy
-import logging
-from selenium.webdriver.remote.remote_connection import LOGGER
-from selenium.common.exceptions import *
-from selenium.webdriver.support import expected_conditions as CondicaoExperada
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from scrapy.selector import Selector
-from time import sleep
-
-
-def iniciar_driver():
-    chrome_options = Options()
-    LOGGER.setLevel(logging.WARNING)
-    arguments = ['--lang=pt-BR', '--window-size=1920,1080', '--headless']
-    for argument in arguments:
-        chrome_options.add_argument(argument)
-
-    chrome_options.add_experimental_option('prefs', {
-        'download.prompt_for_download': False,
-        'profile.default_content_setting_values.notifications': 2,
-        'profile.default_content_setting_values.automatic_downloads': 1,
-
-    })
-    driver = webdriver.Chrome(service=ChromeService(
-        ChromeDriverManager().install()), options=chrome_options)
-
-    wait = WebDriverWait(
-        driver,
-        10,
-        poll_frequency=1,
-        ignored_exceptions=[
-            NoSuchElementException,
-            ElementNotVisibleException,
-            ElementNotSelectableException,
-        ]
-    )
-    return driver, wait
 
 
 class IndeedSpider(scrapy.Spider):
@@ -51,20 +10,32 @@ class IndeedSpider(scrapy.Spider):
         # não esqueça de setar ROBOTSTXT_OBEY = False dentro do arquivo settings.py
         urls = ['https://br.indeed.com/jobs?q=python&l=&from=searchOnHP&vjk=467f2bd1131d76e0']
         for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse, meta={'proximo_url': url})
+            yield scrapy.Request(url=url, callback=self.parse)
     # Response
 
     def parse(self, response):
-        driver, wait = iniciar_driver()
-        driver.get(response.meta['proximo_url'])
-        sleep(10)
-        response_webdriver = Selector(text=driver.page_source)
 
-        for elemento in response.xpath("//div[@class='job_seen_beacon']"):
+        for elemento in response.xpath("//td[@class='resultContent']"):
             yield {
-                'cargo': elemento.xpath(".//span[@id='jobTitle-467f2bd1131d76e0']/text()").get(),
+                'cargo': elemento.xpath(".//span[1]/text()").get(),
                 'nome da empresa': elemento.xpath(".//span[@class='css-1x7z1ps eu4oa1w0']/text()").get(),
                 'local': elemento.xpath(".//div[@class='css-t4u72d eu4oa1w0']/text()").get(),
-                'link da vaga': elemento.xpath(".//a[@id='sj_467f2bd1131d76e0']").get()
+                'link da vaga': 'https://br.indeed.com' + elemento.xpath(".//a/@href").get()
             }
-        driver.close()
+        try:
+            link_proxima_pagina = response.xpath("//a[data-testid='pagination-page-next']/@href").get()
+            print('*'*10)
+            print(link_proxima_pagina)
+            print('*'*10)
+            if link_proxima_pagina is not None:
+                link_completo = 'https://br.indeed.com' + link_proxima_pagina
+                print('*'*10)
+                print(link_completo)
+                print('*'*10)
+                yield scrapy.Request(url=link_completo, callback=self.parse)
+        except Exception as error:
+            print(error)
+            print('Chegamos na última página')
+            
+
+        
